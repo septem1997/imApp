eda01cf104375e072f81276f2f20adb098110221
lambda1997 <750627595@qq.com>
2019-07-05 16:04:02 +0800

    update

--- src/renderer/components/UserChatLog/index.tsx
+++ src/renderer/components/UserChatLog/index.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
-import sty from './index.sass'
+import sty from './index.sass';
+import moment from 'moment';
+import { Radio } from 'antd';
 
 export default class extends React.Component {
   render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
     return <div className={sty.userChatLog}>
+      <div className={sty.head}>
+        <Radio.Group defaultValue="a" buttonStyle="solid" className={sty.group}>
+          <Radio.Button value="a" className={sty.btn}>全部</Radio.Button>
+          <Radio.Button value="f" className={sty.btn}>文件</Radio.Button>
+        </Radio.Group>
+        <input placeholder={'请输入关键词'} className={sty.search}/>
+      </div>
+      <div className={sty.logList}>
+      </div>
     </div>;
   }
 }
--- src/renderer/components/UserOrders/index.tsx
+++ src/renderer/components/UserOrders/index.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
-import sty from './index.sass'
+import sty from './index.sass';
+import moment from 'moment';
 
 export default class extends React.Component {
+
+  state = {
+    orderList: [
+      {
+        orderNo: '123456',
+        status: 0,
+        company: '阿里',
+        time: new Date().getTime() / 1000,
+      },
+      {
+        orderNo: '123456',
+        status: 0,
+        company: '腾讯',
+        time: new Date().getTime() / 1000,
+      },
+    ],
+  };
+
+  statusMap = [
+    '待付款',
+  ];
+
   render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
     return <div className={sty.userOrders}>
+      <div className={sty.head}>
+        <input placeholder={'请输入订单号'} className={sty.search}/>
+      </div>
+      <div className={sty.orderList}>
+        {this.state.orderList.map((order) =>
+          <div className={sty.orderItem}>
+            <div className={sty.head}>
+              <span>订单编号：{order.orderNo}</span>
+              <span className={sty.status}>{this.statusMap[order.status]}</span>
+            </div>
+            <div>采购企业：{order.company}</div>
+            <div>创建时间：{moment.unix(order.time).format('YYYY-MM-DD HH:mm:ss')}</div>
+          </div>
+        )}
+      </div>
     </div>;
   }
 }
